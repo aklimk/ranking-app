d49f48c1f782aeab2503abe8e23bdc1011916f7a
Andrew Klimenko <andrewklimko@outlook.com>
2026-02-18 09:40:29 +1000

    feat: added additional api coverage tests.

--- viz/api/src/database.ts
+++ viz/api/src/database.ts
@@ -7,4 +7,5 @@ export const pool = new Pool({
   database: forceEnvVar(process.env["PG_DB"]),
   user: forceEnvVar(process.env["PG_USER"]),
   password: forceEnvVar(process.env["PG_PASSWORD"]),
+  allowExitOnIdle: true,
 });
